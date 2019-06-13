ba693cffadfb1f16f8df03d41272200131d35e71
GabrielaKlabunde <50932960+GabrielaKlabunde@users.noreply.github.com>
2019-06-13 19:44:30 -0300

    Loop base sobre While

---
+++ LoopWhileBásico.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+        
+       int contador = 0;
+       while(contador <500000){
+           contador++;
+           System.out.println(contador);
+           contador++;
+       }
+    }
+    
+}
