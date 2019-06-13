1f51713f794f67bd090cdbfde1e4233a30fe8e8c
GabrielaKlabunde <50932960+GabrielaKlabunde@users.noreply.github.com>
2019-06-13 19:39:32 -0300

    Exercício 3 do Senai ( Wagner ) sobre loops.

---
+++ NúmerosInteiros.java
@@ -0,0 +1,14 @@
+package javaapplication4;
+
+import java.util.Scanner;
+
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+    Scanner leia = new Scanner (System.in);
+    
+    //ler o valor N 
+    int n = 115;  
+    //numeros inteiro do 1 ao valorN
+    for (int i = 1; i <= n; i++)
+    System.out.println(i);
