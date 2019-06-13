0b308a88d043aa677806be217946ee2d0dafaead
GabrielaKlabunde <50932960+GabrielaKlabunde@users.noreply.github.com>
2019-06-13 19:27:36 -0300

    Trabalho Wagner sobre loop. Exercício 1

---
+++ OrdemCrescente.java
@@ -0,0 +1,15 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+   // 1 a 10 crescente
+    for ( int i = 0; i <= 10; i ++){
+    System.out.println(i);    
+    }
+    
+    }
+    
+}
